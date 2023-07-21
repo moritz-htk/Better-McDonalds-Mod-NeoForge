2643c37ac8ae24e27e47e1f8eb8f18f23545ae41
simon-mc28 <hartkopfmoritz5@gmail.com>
2023-07-21 13:16:51 +0200

    Added creative mode tab

--- src/main/java/net/moritz_htk/bettermcdonaldsmod/BetterMcDonaldsMod.java
+++ src/main/java/net/moritz_htk/bettermcdonaldsmod/BetterMcDonaldsMod.java
@@ -1,10 +1,12 @@
 package net.moritz_htk.bettermcdonaldsmod;
 
 import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.moritz_htk.bettermcdonaldsmod.block.BMMBlocks;
+import net.moritz_htk.bettermcdonaldsmod.item.BMMCreativeModeTabs;
 import net.moritz_htk.bettermcdonaldsmod.item.BMMItems;
 
 @Mod(BetterMcDonaldsMod.MOD_ID)
@@ -16,7 +18,38 @@ public class BetterMcDonaldsMod {
 
         BMMItems.register(modEventBus);
         BMMBlocks.register(modEventBus);
+        BMMCreativeModeTabs.register(modEventBus);
 
         MinecraftForge.EVENT_BUS.register(this);
+
+        modEventBus.addListener(this::addCreative);
+    }
+
+    private void addCreative(BuildCreativeModeTabContentsEvent event) {
+        if (event.getTab() == BMMCreativeModeTabs.BETTER_MCDONALDS_MOD_TAB.get()) {
+            event.accept(BMMBlocks.SALT_BLOCK);
+            event.accept(BMMItems.SALT);
+            event.accept(BMMItems.CHEESE);
+            event.accept(BMMItems.BEEF_PATTY);
+            event.accept(BMMItems.COOKED_BEEF_PATTY);
+            event.accept(BMMItems.MAYONNAISE);
+            event.accept(BMMItems.SWEET_SOUR_SAUCE);
+            event.accept(BMMItems.KETCHUP);
+            event.accept(BMMItems.HAMBURGER);
+            event.accept(BMMItems.CHEESEBURGER);
+            event.accept(BMMItems.BIG_MAC);
+            event.accept(BMMItems.CHICKENBURGER);
+            event.accept(BMMItems.MCCHICKEN);
+            event.accept(BMMItems.FILET_O_FISH);
+            event.accept(BMMItems.CHICKEN_MCNUGGETS);
+            event.accept(BMMItems.FRIES);
+            event.accept(BMMItems.HAPPY_MEAL);
+            event.accept(BMMItems.COCA_COLA);
+            event.accept(BMMItems.FANTA);
+            event.accept(BMMItems.SPRITE);
+            event.accept(BMMItems.LIPTON_ICE_TEA_PEACH);
+            event.accept(BMMItems.MCFLURRY);
+            event.accept(BMMItems.KNIFE);
+        }
     }
 }
---
+++ src/main/java/net/moritz_htk/bettermcdonaldsmod/item/BMMCreativeModeTabs.java
@@ -0,0 +1,24 @@
+package net.moritz_htk.bettermcdonaldsmod.item;
+
+import net.minecraft.core.registries.Registries;
+import net.minecraft.network.chat.Component;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.ItemStack;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.RegistryObject;
+import net.moritz_htk.bettermcdonaldsmod.BetterMcDonaldsMod;
+
+public class BMMCreativeModeTabs {
+    public static final DeferredRegister<CreativeModeTab> CREATIVE_MODE_TABS =
+            DeferredRegister.create(Registries.CREATIVE_MODE_TAB, BetterMcDonaldsMod.MOD_ID);
+
+    public static RegistryObject<CreativeModeTab> BETTER_MCDONALDS_MOD_TAB =
+            CREATIVE_MODE_TABS.register("better_mcdonalds_mod_tab", () -> CreativeModeTab.builder()
+                    .icon(() -> new ItemStack(BMMItems.HAPPY_MEAL.get()))
+                    .title(Component.literal("Better McDonald's Mod")).build());
+
+    public static void register(IEventBus eventBus) {
+        CREATIVE_MODE_TABS.register(eventBus);
+    }
+}
